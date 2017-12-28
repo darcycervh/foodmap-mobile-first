3a6011244f7e38a72c326606678e87c218f14ec8
darcycervh <darcycervera@gmail.com>
2017-12-28 16:36:06 -0500

    estableciendo data

---
+++ js/data.js
@@ -0,0 +1,98 @@
+var data = {
+    'MIRAFLORES': {
+        'restaurantes':{
+            'seafood': [
+            {
+                'name': 'Costazul Seafood',
+                'photo': 'assets/images/costazul.png',
+                'address': 'Calle Berlin 899, Miraflores 15074',
+                'web': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d1157098-Reviews-Costazul_Seafood-Lima_Lima_Region.html',
+                'calification': 4.5,
+                'latitude': -12.1201173,
+                'longitude': -77.03899150000001
+            },
+            {
+               'name': 'La casa del ceviche',
+               'photo':'assets/images/ceviche.png',
+               'address': 'Calle Berlin 317, Miraflores 15074',
+               'web': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d4102395-Reviews-La_casa_del_Ceviche-Lima_Lima_Region.html',
+               'calification': 4.3,
+               'latitude': -12.1213789,
+               'longitude': -77.03277000000003
+            },
+            {
+                'name': 'Punto Azul',
+                'photo':'assets/images/azul.png',
+                'address': 'Calle San Martin 595, Miraflores 15074',
+                'web': 'http://puntoazulrestaurante.com/',
+                'calification': 4,
+                'latitude': -12.1264393,
+                'longitude': -77.02816559999997
+            },
+            {
+                'name': 'Costanera 700',
+                'photo':'assets/images/costanera.png',
+                'address':'Av del Ejército 421, Miraflores 15074',
+                'web':'http://www.costanera700.pe/',
+                'calification': 4.3,
+                'latitude': -12.1166202,
+                'longitude': -77.04526220000002
+            },
+            {
+                'name': 'Restaurante Alfresco',
+                'photo':'assets/images/alfresco.png',
+                'address': 'Malecón Balta 790, Miraflores',
+                'web': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d775152-Reviews-Restaurante_Alfresco-Lima_Lima_Region.html',
+                'calification': 4.2,
+                'latitude': -12.1242546,
+                'longitude': -77.03301199999999
+            },
+            {
+                'name': 'Aventuras Marinas',
+                'photo': 'assets/images/aventuras.png',
+                'address': 'Calle Manuel Bonilla 178, Miraflores 15074',
+                'web': 'https://www.tripadvisor.com.pe/Restaurant_Review-g294316-d10385880-Reviews-Aventuras_Marinas-Lima_Lima_Region.html',
+                'calification': 4.2,
+                'latitude': -12.1199494,
+                'longitude': -77.0271626
+            },
+            {
+                'name': 'La Rosa Nautica',
+                'photo': 'assets/images/rosa.png',
+                'address': 'Espigón Miraflores, Lima 18, Circuito de Playas, Miraflores',
+                'web': 'https://www.larosanautica.com/',
+                'calification': 4.5,
+                'latitude': -12.131053,
+                'longitude': -77.03587900000002
+            },
+            {
+                'name': 'Barra Maretazo',
+                'photo': 'assets/images/barra.png',
+                'address': 'Calle Alcanfores 373, Miraflores 15074',
+                'web': 'http://www.barramaretazo.com/',
+                'calification': 4.5,
+                'latitude': -12.1229558,
+                'longitude': -77.02775199999996
+            },
+            {
+                'name': 'Brujas de Cachiche',
+                'photo': 'assets/images/brujas.png',
+                'address': 'Bolognesi 472, Miraflores 15074',
+                'web': 'http://www.brujasdecachiche.com.pe/',
+                'calification': 4.5,
+                'latitude': -12.1229558,
+                'longitude': -77.02775199999996
+            },
+            {
+                'name': 'Pescados Capitales',
+                'photo': 'assets/images/pescados.png',
+                'address': 'Av Mariscal La Mar 1337, Miraflores 15074',
+                'web': 'http://pescadoscapitales.com/es/',
+                'calification': 4.5,
+                'latitude': -12.1094481,
+                'longitude': -77.04964799999999
+            }
+            ]
+        }
+    }
+}
